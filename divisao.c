49cb5f749472934e43cec7cb185d13ffe95d09ef
pedrocb12 <162707193+pedrocb12@users.noreply.github.com>
2025-11-18 20:15:39 -0300

    add

--- divisao.c
+++ divisao.c
@@ -1,25 +1,9 @@
 #include <stdio.h>
 
-int main() {
-    int dividendo, divisor, quociente = 0;
-
-    printf("Digite o dividendo: ");
-    scanf("%d", &dividendo);
-
-    printf("Digite o divisor: ");
-    scanf("%d", &divisor);
-
-    if (divisor <= 0) {
-        printf("Divisor deve ser maior que zero.\n");
-        return 1;
+int dividir(int a, int b) {
+    if (b == 0) {
+        printf("Erro: divisao por zero nao permitida.\n");
+        return 0;
     }
-
-    int resto = dividendo;
-    while (resto >= divisor) {
-        quociente++;
-        resto -= divisor;
-    }
-
-    printf("Quociente: %d\nResto: %d\n", quociente, resto);
-    return 0;
+    return a / b;
 }
